7a8b53f4c876c23982c97d3c83d3921835b6c325
manci <javiercaliescali@gmail.com>
2021-03-26 14:43:47 -0500

    cors header

--- index.js
+++ index.js
@@ -17,13 +17,6 @@ app.use(express.json({ limit: '1mb' }));
 
 app.use(require('./server/routes/vehiculo'));
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
-     next();
-});
-
 mongoose.connect(process.env.URLDB, 
     { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true, useFindAndModify: false }, 
     (err, res) => {
