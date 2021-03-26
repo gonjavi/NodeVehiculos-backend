9d667264769a6c0fdfb56500dfa7d0cddccf199a
manci <javiercaliescali@gmail.com>
2021-03-26 14:01:40 -0500

    access control origin

--- index.js
+++ index.js
@@ -2,12 +2,18 @@ require('./server/config/config');
 const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
-const cors = require('cors');
-app.use(cors());
+
 app.use(express.urlencoded({ limit: '1mb', extended: true }));
 app.use(express.json({ limit: '1mb' }));
 
 app.use(require('./server/routes/vehiculo'));
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+     next();
+});
 
 mongoose.connect(process.env.URLDB, 
     { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true, useFindAndModify: false }, 
