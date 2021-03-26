475f48813aaf06c1d631b866a0a492792e3f40ca
manci <javiercaliescali@gmail.com>
2021-03-26 14:17:36 -0500

    access control

--- index.js
+++ index.js
@@ -2,6 +2,15 @@ require('./server/config/config');
 const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
+const cors = require('cors');
+app.use(cors());
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+     next();
+});
 
 app.use(express.urlencoded({ limit: '1mb', extended: true }));
 app.use(express.json({ limit: '1mb' }));
