5eea0b1c85bf49345e6f6ac10560710cff3507a1
manci <javiercaliescali@gmail.com>
2021-03-30 09:47:53 -0500

    -

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ require('./server/config/config');
 const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
+const cors = require('cors');
+
+app.use(cors());
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', 'https://react-vehiculos.herokuapp.com');
   res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
   res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
