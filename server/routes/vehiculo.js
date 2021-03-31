aaac855570d8b74e177b0decf0010b2c3545e60e
manci <javiercaliescali@gmail.com>
2021-03-31 09:53:23 -0500

    tamaño foto 20megas

--- server/routes/vehiculo.js
+++ server/routes/vehiculo.js
@@ -1,7 +1,8 @@
 const express = require('express');
 let app = express();
-const cors = require('cors');
-app.use(cors());
+app.use(express.urlencoded({ limit: '20mb', extended: true }));
+app.use(express.json({ limit: '20mb' }));
+
 let Vehiculo = require('../models/vehiculo');
 
 app.get('/vehiculos', (req, res) => {
